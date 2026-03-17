82559275d2ceae9d11670973a27abe64d4634e3d
Olusoga-Peters Oladeji <oladejipeters@gmail.com>
2026-03-17 15:40:32 +0100

    error corrected

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
+const input = document.querySelector("#userInput");
+const playBtn = document.querySelector("#playBtn");
+const userSquare = document.querySelector("#userSquare");
+const randomSquare = document.querySelector("#randomSquare");
+const form = document.querySelector("form");
+const message = document.querySelector("#message");
 
+function clearInput() {
+  input.value = "";
+}
 
-    const input = document.querySelector("#userInput");
-    const playBtn = document.querySelector("#playBtn");
-    const userSquare = document.querySelector("#userSquare");
-    const randomSquare = document.querySelector("#randomSquare");
-    const message = document.querySelector("#message");
+form.addEventListener("submit", function (e) {
+  e.preventDefault() //prevents reloading
+  const userValue = Number(input.value);
 
-    function clearInput() {
-      input.value = "";
-    }
+  if (!userValue || userValue < 1 || userValue > 100) {
+    message.textContent = "Enter a number between 1 and 100.";
+    return;
+  }
 
-    playBtn.addEventListener('click', function () {
-      const userValue = Number(input.value);
+  userSquare.textContent = userValue;
+  userSquare.classList.add("flash");
+  setTimeout(() => userSquare.classList.remove("flash"), 300);
 
-      if (!userValue || userValue < 1 || userValue > 100) {
-        message.textContent = "Enter a number between 1 and 100.";
-        return;
-      }
+  const randomNumber = Math.floor(Math.random() * 100);
+  randomSquare.textContent = randomNumber;
+  randomSquare.classList.add("random-flash");
+  setTimeout(() => randomSquare.classList.remove("random-flash"), 300);
 
-      userSquare.textContent = userValue;
-      userSquare.classList.add("flash");
-      setTimeout(() => userSquare.classList.remove("flash"), 300);
-
-      const randomNumber = Math.floor(Math.random() * 100);
-      randomSquare.textContent = randomNumber;
-      randomSquare.classList.add("random-flash");
-      setTimeout(() => randomSquare.classList.remove("random-flash"), 300);
-
-      if (userValue === randomNumber) {
-        message.textContent = "🎉 You Won!";
-        message.style.color = "lime";
-        clearInput()
-      } else {
-        message.textContent = "❌ Try Again!";
-        message.style.color = "orange";
-        clearInput()
-      }
-    });
+  if (userValue === randomNumber) {
+    message.textContent = "🎉 You Won!";
+    message.style.color = "lime";
+    clearInput();
+  } else {
+    message.textContent = "❌ Try Again!";
+    message.style.color = "orange";
+    clearInput();
+  }
+});
